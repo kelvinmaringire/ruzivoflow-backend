0f8ad13ef916074c472a8ea4b4fbb7bafe659c1d
kelvinmaringire <kelvinmaringire@gmail.com>
2024-05-01 11:46:34 +0200

    admin url ""

--- thedatamine/urls.py
+++ thedatamine/urls.py
@@ -10,7 +10,7 @@ from search import views as search_views
 
 urlpatterns = [
     path("django-admin/", admin.site.urls),
-    path("admin/", include(wagtailadmin_urls)),
+    path("", include(wagtailadmin_urls)),
     path("documents/", include(wagtaildocs_urls)),
     path("search/", search_views.search, name="search"),
 ]
